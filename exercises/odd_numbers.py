7d70e43815a0a7addda0b79bfc1e644dfe4f7111
Abdulmalik Abdulwahab <Abdulmalik Abdulwahab>
2016-02-16 12:35:21 +0100

    Odd Numbers added

---
+++ exercises/odd_numbers.py
@@ -0,0 +1,10 @@
+def odd_numbers(low, high):
+    if type(low) != int or type(high) != int:
+        print("Only Intergers are allowed as parameter")
+        return
+    if low > high :
+        print("lower bound cannot be greater than the higher bound")
+        return
+    for i in range(low, high):
+        if i%2 != 0:
+            print(i)
