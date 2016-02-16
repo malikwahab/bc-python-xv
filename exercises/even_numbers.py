d30d740cbc9c59cb5b3bab5447ce046373e95506
Abdulmalik Abdulwahab <Abdulmalik Abdulwahab>
2016-02-16 12:47:59 +0100

    Zero fixed for even numbers

--- exercises/even_numbers.py
+++ exercises/even_numbers.py
@@ -1,13 +1,13 @@
 def even_numbers(low, high):
-	if type(low) != int or type(high) != int:
-		print("Only Intergers are allowed as parameter")
-		return
-	if low > high :
-		print("lower bound cannot be greater than the higher bound")
-		return
-
-	for i in range(low, high):
-		if i%2 == 0 :
-			print(i)
-
+    if type(low) != int or type(high) != int:
+        print("Only Intergers are allowed as parameter")
+        return
+    if low > high :
+        print("lower bound cannot be greater than the higher bound")
+        return
+    for i in range(low, high):
+        if i == 0 :
+            continue
+        if i%2 == 0 :
+            print(i)
 even_numbers(0, 10)
