711353e55b53b89990a433782ac83ed441d76742
Abdulmalik Abdulwahab <Abdulmalik Abdulwahab>
2016-02-18 09:41:10 +0100

    Added comment to is_prime

--- exercises/is_prime.py
+++ exercises/is_prime.py
@@ -5,6 +5,7 @@ def is_prime(n):
     if n == 2: 
         return True
     # all other even numbers are not primes
+    # Binary of all even number ends with 0 so even & 0b1 always equal 0
     if not n & 1: 
         return False
     for x in range(3, int(n**0.5)+1, 2):
